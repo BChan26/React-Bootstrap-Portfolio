2ec40717907acfd42d07507a6ea45fa349296026
Brandon Chan <bchan26@bu.edu>
2022-12-21 20:46:57 -0500

    added P3 and template for hackathon and p4

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -1,6 +1,9 @@
 import Forecast from '../assets/img/Forecast.png'
 import Boston1 from '../assets/img/Boston1.png'
 import The65Bus from '../assets/img/The65Bus.png'
+import Hollygram from '../assets/img/Hollygram.png'
+import TickIt from '../assets/img/TickIt.png'
+import ComingSoon from '../assets/img/ComingSoon.png'
 import {ProjectCard} from './ProjectCard'
 
 
@@ -34,6 +37,26 @@ const project3 =[
         URL: 'https://65bus.netlify.app'
     }]
 
+const project4 =[
+    {
+        // title: "Holiday Instagram Clone",
+        // description: "PERN Full Stack w/ CRUD & Auth",
+        imgURL: Hollygram,
+        URL: 'https://hollygram.netlify.app/'
+    }]
+
+const project5 =[
+    {
+        imgURL: TickIt,
+        // URL: ''
+    }]
+
+const project6 =[
+    {
+        imgURL: ComingSoon,
+        // URL: ''
+    }]
+
 return (
     <section className = "projects" id="projects">
         <Container>
@@ -43,33 +66,60 @@ return (
                     <Tab.Container id="project-table" defaultActiveKey="first">
                         
                         <Nav variant="pills" defaultActiveKey="/home">
+                        
                         <Nav.Item>
-                            <Nav.Link eventKey="first">MBTA 65 Bus Tracker
+                            <Nav.Link eventKey="first">Holiday Instagram
                             <h6>↓</h6>
-                            <h6>React, Axios Calls</h6>
+                            <h6>PERN Full-Stack</h6>
+                            </Nav.Link>
+                            
+                        </Nav.Item>
+                        
+                        <Nav.Item>
+                            <Nav.Link eventKey="second">MBTA 65 Bus Tracker
+                            <h6>↓</h6>
+                            <h6>React w/ Axios Calls</h6>
                             </Nav.Link>
                             
                         </Nav.Item>
 
                         <Nav.Item>
-                            <Nav.Link eventKey="second">Weather Forecast
+                            <Nav.Link eventKey="third">Weather Forecast
                             <h6>↓</h6>
                             <h6>HTML, CSS, Javascript</h6>
                             </Nav.Link>
                         </Nav.Item>
 
                         <Nav.Item>
-                            <Nav.Link eventKey="third">Boston1 Dragon Boat
+                            <Nav.Link eventKey="fourth">Boston1 Dragon Boat
                             <h6>↓</h6>
                             <h6>WordPress</h6>
                             </Nav.Link>
                         </Nav.Item>
+
+                        <Nav.Item>
+                            <Nav.Link eventKey="fifth">Hackathon Project
+                            <h6>↓</h6>
+                            <h6>Python & Django w/ Auth</h6>
+                            </Nav.Link>
+                        </Nav.Item>
+
+                        <Nav.Item>
+                            <Nav.Link eventKey="sixth">Teacher Documentation Tool
+                            <h6>↓</h6>
+                            <h6>Completed by January 6th, 2023</h6>
+                            </Nav.Link>
+                        </Nav.Item>
+
                         </Nav>
 
+                        
+
                         <Tab.Content>
+                            
                             <Tab.Pane eventKey="first">
                                 <Row>
-                                    {project3.map((project, index) => {
+                                    {project4.map((project, index) => {
                                         return (
                                             <ProjectCard
                                             key={index}
@@ -83,6 +133,20 @@ return (
 
                             <Tab.Pane eventKey="second">
                                 <Row>
+                                    {project3.map((project, index) => {
+                                        return (
+                                            <ProjectCard
+                                            key={index}
+                                            {...project}/>
+                                            
+                                        )
+                                    })
+                                    }
+                                </Row>
+                            </Tab.Pane>
+
+                            <Tab.Pane eventKey="third">
+                                <Row>
                                 {project1.map((project, index) => {
                                         return (
                                             <ProjectCard
@@ -94,7 +158,7 @@ return (
                                 </Row>
                             </Tab.Pane>
 
-                            <Tab.Pane eventKey="third">
+                            <Tab.Pane eventKey="fourth">
                                 <Row>
                                 {project2.map((project, index) => {
                                         return (
@@ -106,6 +170,34 @@ return (
                                     }
                                 </Row>
                             </Tab.Pane>
+
+                            <Tab.Pane eventKey="fifth">
+                                <Row>
+                                {project5.map((project, index) => {
+                                        return (
+                                            <ProjectCard
+                                            key={index}
+                                            {...project}/>
+                                        )
+                                    })
+                                    }
+                                </Row>
+                            </Tab.Pane>
+
+                            <Tab.Pane eventKey="sixth">
+                                <Row>
+                                {project6.map((project, index) => {
+                                        return (
+                                            <ProjectCard
+                                            key={index}
+                                            {...project}/>
+                                        )
+                                    })
+                                    }
+                                </Row>
+                            </Tab.Pane>
+
+
                         </Tab.Content>
 
                     </Tab.Container>
